24d7fdae8a9de703985e0d92af3ac4bbef7de08d
Waspoedertje <mail_van_mark@hotmail.com>
2026-07-05 09:29:28 +0200

    Refactor Plex class to use PlexAPI

--- Plex.py
+++ Plex.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-
-import requests
-import xml.etree.ElementTree as ET
-
+from plexapi.server import PlexServer
 from config import get
 
 
@@ -10,176 +6,37 @@ class Plex:
 
     def __init__(self):
 
-        self.base_url = get("PLEX_BASEURL").rstrip("/")
-        self.token = get("PLEX_TOKEN")
-
-        self.headers = {
-            "Accept": "application/xml"
-        }
-
-    ############################################################
-
-    def _get(self, endpoint):
-
-        url = f"{self.base_url}{endpoint}"
-
-        r = requests.get(
-            url,
-            headers=self.headers,
-            params={
-                "X-Plex-Token": self.token
-            },
-            timeout=30
+        self.server = PlexServer(
+            get("PLEX_BASEURL"),
+            get("PLEX_TOKEN")
         )
 
-        r.raise_for_status()
-
-        return r.text
-
-    ############################################################
-
-    def validate(self):
-
-        try:
-
-            self._get("/")
-
-            return True
-
-        except Exception:
-
-            return False
-
-    ############################################################
-
     def server_name(self):
-
-        xml = self._get("/")
-
-        root = ET.fromstring(xml)
-
-        return root.attrib.get("friendlyName")
-
-    ############################################################
+        return self.server.friendlyName
 
     def version(self):
-
-        xml = self._get("/")
-
-        root = ET.fromstring(xml)
-
-        return root.attrib.get("version")
-
-    ############################################################
+        return self.server.version
 
     def libraries(self):
 
-        xml = self._get("/library/sections")
+        libs = []
 
-        root = ET.fromstring(xml)
+        for lib in self.server.library.sections():
 
-        libraries = []
-
-        for library in root.findall("Directory"):
-
-            libraries.append({
-
-                "id": library.attrib["key"],
-
-                "name": library.attrib["title"],
-
-                "type": library.attrib["type"]
-
+            libs.append({
+                "name": lib.title,
+                "type": lib.type
             })
 
-        return libraries
+        return libs
 
-    ############################################################
+    def users(self):
 
-    def shared_users(self):
-
-        r = requests.get(
-
-            "https://plex.tv/api/users",
-
-            params={
-
-                "X-Plex-Token": self.token
-
-            },
-
-            timeout=30
-
-        )
-
-        r.raise_for_status()
-
-        root = ET.fromstring(r.text)
-
-        users = []
-
-        for user in root.findall("User"):
-
-            users.append({
-
-                "id": user.attrib.get("id"),
-
-                "username": user.attrib.get("username"),
-
-                "email": user.attrib.get("email"),
-
-                "title": user.attrib.get("title")
-
-            })
-
-        return users
-
-    ############################################################
+        return self.server.myPlexAccount().users()
 
     def usernames(self):
 
-        names = []
-
-        for user in self.shared_users():
-
-            username = user["username"]
-
-            if username:
-
-                names.append(username)
-
-        return sorted(names, key=str.lower)
-
-    ############################################################
-
-    def print_summary(self):
-
-        print()
-
-        print("Server :", self.server_name())
-
-        print("Version:", self.version())
-
-        print()
-
-        print("Libraries")
-
-        print("----------------")
-
-        for library in self.libraries():
-
-            print(
-
-                f'{library["name"]}'
-
-                f' ({library["type"]})'
-
-            )
-
-        print()
-
-        print(
-
-            f"Shared Users: {len(self.shared_users())}"
-
+        return sorted(
+            [u.username for u in self.users()],
+            key=str.lower
         )
